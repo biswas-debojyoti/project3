2ba8b99d80f17d8c1982d1e476258e06ddb6af90
debojyoti_biswas <debojyotibiswas00@gmail.com>
2023-03-04 23:25:44 +0530

    card_section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -34,7 +34,37 @@ export default function Home() {
   </button>
 </div>
 
-    
+
+
+
+
+
+{/* 
+<div class="card" style="width: 18rem;">
+  <img src={image1} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src={image1} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div><div class="card" style="width: 18rem;">
+  <img src={image1} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> */}
+
+
     </>
    
   )
